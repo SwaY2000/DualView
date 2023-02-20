58cc26042b0191be175d4d4e3c9c14f9f01b89b7
Данил Рощенко <dan.frais2000@gmail.com>
2023-02-21 01:07:07 +0200

    feat-1/ create manager and abstract user model

--- user/managers.py
+++ user/managers.py
@@ -1,29 +1,29 @@
 from django.contrib.auth.base_user import BaseUserManager
-from django.utils.translation import gettext as _
 
-class CustomUserManager(BaseUserManager):
-    """
-    Custom user model manager where email is the unique identifier
-    for authentication instead of usernames.
-    """
 
-    def create_user(self, email, password, **extra_fields):
-        if not email:
-            raise
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        if username is None:
+            raise TypeError('Users must have a username.')
+
+        if email is None:
+            raise TypeError('Users must have an email address.')
+
+        user = self.model(username=username, email=self.normalize_email(email))
         user.set_password(password)
         user.save()
+
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
+    def create_superuser(self, username, email, password):
+        if password is None:
+            raise TypeError('Superusers must have a password.')
 
-        if extra_fields.get('is_staff') is not True:
-            raise
-        if extra_fields.get('is_superuser') is not True:
-            raise
-        return self.create_user(email, password, **extra_fields)
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
 
--- user/models.py
+++ user/models.py
@@ -1,17 +1,22 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
-from django.utils.translation import gettext as _
 
-from .managers import CustomUserManager
+from user.managers import UserManager
 
-class CustomUser(AbstractUser):
-    email = models.EmailField(_('email address'), unique=True)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ('username',)
+    REQUIRED_FIELDS = ['username']
 
-    objects = CustomUserManager()
+    objects = UserManager()
 
     def __str__(self):
         return self.email
-
