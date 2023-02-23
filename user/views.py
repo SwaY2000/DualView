44e6fd9c17489ad1872cf80bfef189e6df536be1
Данил Рощенко <dan.frais2000@gmail.com>
2023-02-23 03:13:48 +0200

    feat-5/add view for inviteuser

--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import views
 
-# Create your views here.
+from .serializer import InviteUserSerializer
+from .tasks import send_verification_code
+
+
+class InviteUserView(views.APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = InviteUserSerializer(data=request.data)
+        if serializer.is_valid():
+            instance = serializer.save()
+            send_verification_code(instance)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+
