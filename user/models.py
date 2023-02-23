22117c8a1bec53a136806e4b507b801f9d424ffa
Данил Рощенко <dan.frais2000@gmail.com>
2023-02-23 03:12:25 +0200

    feat-5/add functional with save for generate random digit

--- user/models.py
+++ user/models.py
@@ -1,18 +1,21 @@
+import random
+
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
 
-from user.managers import UserManager
+from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(db_index=True, max_length=255, unique=True)
+    username = models.CharField(db_index=True, max_length=20, unique=True)
     email = models.EmailField(db_index=True, unique=True)
-    is_active = models.BooleanField(default=True)
+    verification_code = models.IntegerField(null=True)
+    is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    friend = models.ManyToManyField('self', related_name='friends', blank=True)
+    friend = models.ManyToManyField('self', related_name='friends', symmetrical=False, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
@@ -21,3 +24,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.verification_code = random.randint(100000, 999999)
+        super().save(*args, **kwargs)
