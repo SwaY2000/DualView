07cba7b82a6aa2ff5679b35d7e4871c4baa02f40
Данил Рощенко <dan.frais2000@gmail.com>
2023-02-24 17:30:00 +0200

    feat-8/add view for friends

--- user/views.py
+++ user/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import views, status
 
-from .serializer import InviteUserSerializer, ActivateUserSerializer
+from .serializer import InviteUserSerializer, ActivateUserSerializer, UserSerializer
 from .tasks import send_verification_code
 
 
@@ -54,3 +54,33 @@ class ActivateUserView(views.APIView):
         else:
             return Response({'error': 'Email is required.'}, status=status.HTTP_400_BAD_REQUEST)
 
+
+class UserFriend(views.APIView):
+    def post(self, request):
+        friend = get_object_or_404(get_user_model(), id=request.data.get('id', ''))
+        if request.user == friend:
+            return Response({'error': 'You cannot send friend request to yourself'}, status=status.HTTP_400_BAD_REQUEST)
+        request.user.send_friend_request(friend)
+        return Response({'success': 'Friend request sent'}, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        friend = get_object_or_404(get_user_model(), id=request.data.get('id', ''))
+        if friend not in request.user.friend_requests_received.all():
+            return Response({'error': 'Friend request not found'}, status=status.HTTP_404_NOT_FOUND)
+        request.user.accept_friend_request(friend)
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request):
+        friend = get_object_or_404(get_user_model(), id=request.data.get('id', ''))
+
+        if friend not in request.user.friend_requests_received.all() and friend not in request.user.friend.all():
+            return Response({'error': 'User is not your friend'}, status=status.HTTP_404_NOT_FOUND)
+
+        if friend in request.user.friend_requests_received.all():
+            request.user.reject_friend_request(friend)
+        else:
+            request.user.unfriend(friend)
+
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
