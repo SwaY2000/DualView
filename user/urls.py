15df8c986e35db093ea34f16914a8f0c29e65bde
Данил Рощенко <dan.frais2000@gmail.com>
2023-02-24 17:29:28 +0200

    feat-8/add route for friends

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import InviteUserView
+from .views import InviteUserView, ActivateUserView, UserFriend
 
 urlpatterns = [
-    path('user/', InviteUserView.as_view(), name='invite_user'),
+    path('user/', InviteUserView.as_view(), name='user'),
+    path('user/activate/', ActivateUserView.as_view(), name='activate_user'),
+    path('user/friend/', UserFriend.as_view(), name='activate_user'),
     path('user/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('user/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
