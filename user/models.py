d41e7b9d77db04c92d9aa41b04e4b7670ca3c59b
Данил Рощенко <dan.frais2000@gmail.com>
2023-02-24 17:27:44 +0200

    feat-8/add new field for request to frined and create attr for: accept, send, decline, delete friend

--- user/models.py
+++ user/models.py
@@ -16,6 +16,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     friend = models.ManyToManyField('self', related_name='friends', symmetrical=False, blank=True)
+    friend_requests_sent = models.ManyToManyField('self', related_name='friend_requests_sent', symmetrical=False,
+                                                  blank=True)
+    friend_requests_received = models.ManyToManyField('self', related_name='friend_requests_received',
+                                                      symmetrical=False, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
@@ -26,7 +30,29 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.email
 
     def save(self, *args, **kwargs):
-        '''functional create random 6 digits and save to field "verification_code" for invite user'''
+        """functional create random 6 digits and save to field "verification_code" for invite user"""
         if not self.pk:
             self.verification_code = random.randint(100000, 999999)
         super().save(*args, **kwargs)
+
+    def send_friend_request(self, friend):
+        if friend != self and not self.friend.filter(pk=friend.pk).exists() and not self.friend_requests_sent.filter(
+                pk=friend.pk).exists() and not self.friend_requests_received.filter(pk=friend.pk).exists():
+            self.friend_requests_sent.add(friend)
+            friend.friend_requests_received.add(self)
+
+    def accept_friend_request(self, friend):
+        if friend in self.friend_requests_received.all():
+            self.friend_requests_received.remove(friend)
+            self.friend.add(friend)
+            friend.friend.add(self)
+
+    def reject_friend_request(self, friend):
+        if friend in self.friend_requests_received.all():
+            self.friend_requests_received.remove(friend)
+            friend.friend_requests_sent.remove(self)
+
+    def unfriend(self, friend):
+        if friend in self.friend.all():
+            self.friend.remove(friend)
+            friend.friend.remove(self)
