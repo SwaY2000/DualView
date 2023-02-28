52b95522af05fba22d5be6141550e0e0bd9e2339
Данил Рощенко <dan.frais2000@gmail.com>
2023-02-28 21:40:29 +0200

    feat-10/setup settings for save video and photo to aws, add in requirements django storage, create view for create Video object, serializer, add route

--- room/views.py
+++ room/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status, views
+from rest_framework.parsers import MultiPartParser
+from rest_framework.response import Response
+from .serializers import VideoSerializer
 
-# Create your views here.
+
+class VideoView(views.APIView):
+    parser_classes = (MultiPartParser,)
+
+    def post(self, request, format=None):
+        serializer = VideoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
