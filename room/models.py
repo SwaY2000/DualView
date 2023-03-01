a36065891c6ac2a02696f54042a79f688db13aa8
Данил Рощенко <dan.frais2000@gmail.com>
2023-03-01 06:12:02 +0200

    feat-14/add put method in VideoDetailView, also change save method(delete useless functional) and add update method in serializer

--- room/models.py
+++ room/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from storages.backends.s3boto3 import S3Boto3Storage
 
@@ -5,8 +7,13 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class Video(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    video_file = models.FileField(storage=S3Boto3Storage(), upload_to='videos/')
-    poster_image = models.ImageField(storage=S3Boto3Storage(), upload_to='posters/')
+    video_file = models.FileField(storage=S3Boto3Storage(), upload_to='videos/', unique=True)
+    poster_image = models.ImageField(storage=S3Boto3Storage(), upload_to='posters/', unique=True)
 
     def __str__(self):
         return self.title
+
+    @staticmethod
+    def hashing_file_name(name):
+        extension = name.split('.')[-1]
+        return f'{uuid.uuid4().hex}.{extension}'
